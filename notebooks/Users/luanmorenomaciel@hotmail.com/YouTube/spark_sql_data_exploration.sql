b6249939e8aeb83195fe979d061752f405b666d0
Luan Moreno <luanmorenomaciel@hotmail.com>
2019-01-09 17:09:53 -0200

    new code

---
+++ notebooks/Users/luanmorenomaciel@hotmail.com/YouTube/spark_sql_data_exploration.sql
@@ -0,0 +1,12 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC 
+-- MAGIC # Exploração de Dados com SparkSQL
+-- MAGIC 
+-- MAGIC Luan Moreno Medeiros Maciel  
+-- MAGIC One Way Solution
+-- MAGIC 
+-- MAGIC github = 
+
+-- COMMAND ----------
+
